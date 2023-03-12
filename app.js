e0bf7c320e5b671accd1b757bc5848f91b93cbea
Kaisss <105242668+themdkaish@users.noreply.github.com>
2023-03-12 23:36:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function farzi(){
+    window.open("https://dotmovies.xyz/download-farzi-season-1-hindi-amazon-original-complete-series-480p-720p-1080p-2160p-4k/")
+}
+function tu_jhoothi_me_makkar(){
+    window.open("https://dotmovies.cam/download-tu-jhoothi-main-makkaar-2023-hindi-full-movie-v3-hdcamrip-480p-720p-1080p/")
+}
