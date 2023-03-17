fc22cc9c7bd4d7cd6518cf8563aae55c1b1aaee3
Kaisss <105242668+themdkaish@users.noreply.github.com>
2023-03-17 09:51:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 function farzi(){
-    window.open("https://dotmovies.xyz/download-farzi-season-1-hindi-amazon-original-complete-series-480p-720p-1080p-2160p-4k/")
+    window.open("https://new6.gdtot.cfd/file/3324721801")
 }
 function tu_jhoothi_me_makkar(){
-    window.open("https://dotmovies.cam/download-tu-jhoothi-main-makkaar-2023-hindi-full-movie-v3-hdcamrip-480p-720p-1080p/")
+    window.open("https://new6.gdtot.cfd/file/1580843589")
 }
