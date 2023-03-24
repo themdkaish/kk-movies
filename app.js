39300ba123ad06b7e9090ba510b67b49ea4b27c3
Kaisss <105242668+themdkaish@users.noreply.github.com>
2023-03-24 09:59:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,6 @@ function farzi(){
 function tu_jhoothi_me_makkar(){
     window.open("https://new6.gdtot.cfd/file/1580843589")
 }
+function Zwingto{
+    window.open("https://dotmovies.cam/download-zwigato-2023-hdcam-hindi-full-movie-480p-720p-1080p/")
+}
